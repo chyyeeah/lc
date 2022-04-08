45227825ae96eedb59fa85380f7d52e4457c374e
Wilson Cheah <wilsoncheah5@gmail.com>
2022-04-08 11:43:59 -0700

    4/8

--- grokking/KClosestNumbers.js
+++ grokking/KClosestNumbers.js
@@ -1,7 +1,6 @@
 const Heap = require('collections/heap'); //http://www.collectionsjs.com
 
 const find_closest_elements = function (arr, K, X) {
-  result = [];
   const maxHeap = new Heap([...arr], null, (a, b) => Math.abs(a - X) - Math.abs(b - X))
   while (maxHeap.length > K) {
     maxHeap.pop();
@@ -9,7 +8,6 @@ const find_closest_elements = function (arr, K, X) {
   return maxHeap.toArray().sort((a, b) => a - b);
 };
 
-  fgvggb
 console.log(`'K' closest numbers to 'X' are: ${find_closest_elements([5, 6, 7, 8, 9], 3, 7)}`)
 console.log(`'K' closest numbers to 'X' are: ${find_closest_elements([2, 4, 5, 6, 9], 3, 6)}`)
 console.log(`'K' closest numbers to 'X' are: ${find_closest_elements([2, 4, 5, 6, 9], 3, 10)}`)
