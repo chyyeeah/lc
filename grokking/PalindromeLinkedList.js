b47342d4a527151e6b9461d92c17e1873295b2c2
Wilson Cheah <wilsoncheah5@gmail.com>
2022-04-11 20:28:44 -0700

    4/11

--- grokking/PalindromeLinkedList.js
+++ grokking/PalindromeLinkedList.js
@@ -1,5 +1,5 @@
 class Node {
-  constructor(value, next=null){
+  constructor(value, next = null) {
     this.value = value;
     this.next = next;
   }
@@ -7,14 +7,34 @@ class Node {
 
 
 const is_palindromic_linked_list = (head) => {
-  
+  let slow = head;
+  let fast = head;
+  while (fast != null && fast.next != null) {
+    slow = head.next;
+    fast = fast.next.next;
+  }
+  let headOfSecondHalf = reverse(slow);
+  const copyHeadOfSecondHalf = headOfSecondHalf;
+  while (head != null && headOfSecondHalf != null) {
+    // console.log('head', head);
+    // console.log('headOfSecondHalf', headOfSecondHalf);
+    if (head.value !== headOfSecondHalf.value) {
+      break;
+    }
+    head = head.next;
+    headOfSecondHalf = headOfSecondHalf.next;
+  }
+  reverse(copyHeadOfSecondHalf);
+  // console.log('head', head);
+  //   console.log('headOfSecondHalf', headOfSecondHalf);
+  if (head == null || headOfSecondHalf == null) return true;
   return false;
 };
 
 const reverse = (head) => {
   let prev = null;
   let current = head;
-  while (head != null) {
+  while (current != null) {
     const next = current.next;
     current.next = prev;
     prev = current;
@@ -30,8 +50,7 @@ head.next.next = new Node(6)
 head.next.next.next = new Node(4)
 head.next.next.next.next = new Node(2)
 
-console.log(reverse(head));
-// console.log(`Is palindrome: ${is_palindromic_linked_list(head)}`)
+console.log(`Is palindrome: ${is_palindromic_linked_list(head)}`)
 
-// head.next.next.next.next.next = new Node(2)
+head.next.next.next.next.next = new Node(2)
 // console.log(`Is palindrome: ${is_palindromic_linked_list(head)}`)
