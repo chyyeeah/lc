bb0210a3e0ba6422e195ee7ea8df08a8931de603
Wilson Cheah <wilsoncheah5@gmail.com>
2022-04-19 22:52:19 -0700

    4/19

---
+++ grokking/StringPermutationsbychangingcase.js
@@ -0,0 +1,22 @@
+const find_letter_case_string_permutations = function (str) {
+  const permutations = [''];
+  const letters = 'abcdefghijklmnopqrstuvwxyz';
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    let length = permutations.length;
+    while (length > 0) {
+      length--;
+      const perm = permutations.shift();
+      if (letters.indexOf(char) > -1) {
+        permutations.push(perm + char.toUpperCase());
+      }
+      permutations.push(perm + char);
+    }
+  }
+  console.table(permutations)
+  return permutations;
+};
+
+
+console.log(`String permutations are: ${find_letter_case_string_permutations("ad52")}`)
+console.log(`String permutations are: ${find_letter_case_string_permutations("ab7c")}`)
